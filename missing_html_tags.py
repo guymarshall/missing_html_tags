ff97bda5b357b7f11eb67ddc65f7a5adc07c8658
Guy Marshall <guymarshall9@gmail.com>
2022-08-04 21:42:16 +0100

    Change file= to with open as file

--- missing_html_tags.py
+++ missing_html_tags.py
@@ -4,24 +4,21 @@ file_path = input("Enter the filepath: ")
 html_base_words = ["div", "article", "label", "section", "form", "span", "strong", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "li", "header", "i", "button", "select", "option", "table", "thead", "tbody", "tfooter", "th"]
 element_counts = {}
 
-file = open(file_path, "r")
+with open(file_path, "r") as file:
+    file_contents = ""
 
-file_contents = ""
+    for line in file:
+        file_contents += line
 
-for line in file:
-    file_contents += line
+    for word in html_base_words:
+        opening_tag = f"<{word}"
+        closing_tag = f"</{word}"
 
-for word in html_base_words:
-    opening_tag = f"<{word}"
-    closing_tag = f"</{word}"
-    
-    opening_count = file_contents.count(opening_tag)
-    element_counts[opening_tag] = opening_count
-    
-    closing_count = file_contents.count(closing_tag)
-    element_counts[closing_tag] = closing_count
+        opening_count = file_contents.count(opening_tag)
+        element_counts[opening_tag] = opening_count
 
-file.close()
+        closing_count = file_contents.count(closing_tag)
+        element_counts[closing_tag] = closing_count
 
 pprint.pprint(element_counts)
 user_input = input("Press enter to close...")
